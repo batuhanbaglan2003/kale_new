a0af4c3e720b193b5aee0b76aa4ba4ffc3cfdcfe
batuhanbaglan2003 <batuhanbaglan2003@gmail.com>
2026-04-25 13:58:37 +0300

    pezevenk mert

--- deneme/mermi_top_gülle_vs/bullet.gd
+++ deneme/mermi_top_gülle_vs/bullet.gd
@@ -1,11 +1,27 @@
 extends Area2D
 
-var speed = Vector2(700,-900)
+var speed = Vector2.ZERO
 var gravity_force = 1800
 
 func _process(delta):
-	speed.y += gravity_force*delta
+	# Eğer hız atanmışsa hareket et
+	if speed != Vector2.ZERO:
+		# Yer çekimi ekle
+		speed.y += gravity_force * delta
+		# Konumu güncelle (Mesafe = Hız * Zaman)
+		position += speed * delta
+		# Gittiği yöne bakmasını sağla
+		rotation = speed.angle()
+
+# Çarpışma olduğunda çalışır
+func _on_body_entered(body):
+	# Arkadaşının kalelerinde bu fonksiyonun adı 'take_damage' olmalı
+	if body.has_method("take_damage"):
+		body.take_damage(25)
 	
-	position += delta*speed
-	
-	rotation = speed.angle()
+	# Bir şeye çarptığı an mermiyi sil
+	queue_free()
+
+# Ekrandan çıktığında belleği temizle
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
