66b851844ada314460d6d956d18c7a476bfe3fa8
batuhanbaglan2003 <batuhanbaglan2003@gmail.com>
2026-04-25 14:23:02 +0300

    Update bullet.gd

--- deneme/mermi_top_gülle_vs/bullet.gd
+++ deneme/mermi_top_gülle_vs/bullet.gd
@@ -4,7 +4,6 @@ var speed = Vector2.ZERO
 var gravity_force = 1800
 
 func _process(delta):
-<<<<<<< Updated upstream
 	# Eğer hız atanmışsa hareket et
 	if speed != Vector2.ZERO:
 		# Yer çekimi ekle
@@ -18,24 +17,11 @@ func _process(delta):
 func _on_body_entered(body):
 	# Arkadaşının kalelerinde bu fonksiyonun adı 'take_damage' olmalı
 	if body.has_method("take_damage"):
-		body.take_damage(25)
+		body.take_damage(25) # HASAR DEĞERİNİ BURADAN 25 VEYA 300 OLARAK AYARLAYABİLİRSİN
 	
 	# Bir şeye çarptığı an mermiyi sil
 	queue_free()
 
 # Ekrandan çıktığında belleği temizle
-=======
-	if speed != Vector2.ZERO:
-		speed.y += gravity_force*delta
-		position += delta*speed
-		rotation = speed.angle()
-
-func _on_body_entered(body):
-	if body.has_method("take_damage"):
-		body.take_damage(300)
-	
-	queue_free()
-
->>>>>>> Stashed changes
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
